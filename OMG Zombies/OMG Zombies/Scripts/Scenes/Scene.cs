cb525a502ff72318f7dd2034e4e577de437a0a37
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2022-05-20 11:55:37 +0100

    Criar classe base para as cenas

---
+++ OMG Zombies/OMG Zombies/Scripts/Scenes/Scene.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OMG_Zombies.Scripts.Scenes
+{
+    public abstract class Scene
+    {
+        #region Campos e propriedades
+
+        protected Game1 game;
+        protected GraphicsDevice graphicsDevice;
+        protected ContentManager content;
+
+        #endregion
+
+
+        #region métodos
+
+        public abstract void LoadContent();
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public Scene(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            this.game = game;
+            this.graphicsDevice = graphicsDevice;
+            this.content = Game1.ContentManager;
+        }
+
+        public abstract void Update(GameTime gameTime);
+
+        #endregion
+    }
+}
